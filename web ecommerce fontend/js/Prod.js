98177c248e58376236b4a4f691da660f8dde5105
duchaicoder54 <110151336+duchaicoder54@users.noreply.github.com>
2022-08-06 01:52:46 +0700

    upload

--- web ecommerce fontend/js/Prod.js
+++ web ecommerce fontend/js/Prod.js
@@ -1,14 +1,14 @@
 const productsEtl = document.querySelector('#product_list');
-  const productBElm = document.querySelector('.cart-items');
-  const card_p_n = document.querySelector('.card_p_n');
-  const total_sp =document.querySelector('.total_sp');
-  console.log(total_sp)
-  
- 
-  console.log(total_sp)
-  function render_products(){
-products.forEach((product) =>{
-  productsEtl.innerHTML += `
+const productBElm = document.querySelector('.cart-items');
+const card_p_n = document.querySelector('.card_p_n');
+const total_sp = document.querySelector('.total_sp');
+console.log(total_sp)
+
+
+console.log(total_sp)
+function render_products() {
+  products.forEach((product) => {
+    productsEtl.innerHTML += `
         <div id="product_item "  class=" col-sm-6 col-md-4 col-lg-3">   
         <div class="img">
              <img class="anh" src="${product.imgSrc}" alt="${product.name}">
@@ -27,49 +27,50 @@ products.forEach((product) =>{
   </div>
   
     </div>`
-})
+  })
 
-  }
-  render_products()
-  
-
-let card =JSON.parse(localStorage.getItem("Card")) || []
-updateCade()
-function Add_Card(id){
-  if(
-  card.some((Item) =>  Item.id === id)){
-  alert('san pham nay da co trong gio hang r')
-  changenumber("plus", id)
-  }
-else{
-  const Item = products.find((product) => product.id === id
-  
-  )
-  alert('san pham da duoc them vao gio hang')
-  card.push({
-    ...Item,
-    numberproduct : 1})
 }
+render_products()
+
+
+let card = JSON.parse(localStorage.getItem("Card")) || []
+updateCade()
+function Add_Card(id) {
+  if (
+    card.some((Item) => Item.id === id)) {
+    alert('san pham nay da co trong gio hang r')
+    changenumber("plus", id)
+  }
+  else {
+    const Item = products.find((product) => product.id === id
+
+    )
+    alert('san pham da duoc them vao gio hang')
+    card.push({
+      ...Item,
+      numberproduct: 1
+    })
+  }
 
   // card.some((Item) =>{
   //   Item.id === id
   // })
 
-// console.log(card )
-updateCade()
+  // console.log(card )
+  updateCade()
 
 }
-function updateCade(){
+function updateCade() {
   renderCard()
   renderTotalcard()
   localStorage.setItem("Card", JSON.stringify(card));
- 
+
 }
- function renderCard(){
+function renderCard() {
   productBElm.innerHTML = "";
-  card.forEach((Item) =>{
-    productBElm.innerHTML += 
-    `  <div class="cart-item">
+  card.forEach((Item) => {
+    productBElm.innerHTML +=
+      `  <div class="cart-item">
     <div class="Card-infor" onclick="removeCart(${Item.id})">
         <img  src="${Item.imgSrc}" alt="">
         <h4>${Item.name}</h4>
@@ -84,64 +85,64 @@ function updateCade(){
 </div>
 </div> 
     `
-  } )
-}  
-function renderTotalcard(){
+  })
+}
+function renderTotalcard() {
   let total_price = 0,
-  total_product = 0
+    total_product = 0
   card.forEach((Item) => {
-    total_price +=  Item.price * Item.numberproduct;
+    total_price += Item.price * Item.numberproduct;
     total_product += Item.numberproduct
   })
-  card_p_n.innerHTML =`Subtotal(${total_product }product) : $${total_price}`
-  total_sp.innerHTML = `${total_product }`
+  card_p_n.innerHTML = `Subtotal(${total_product}product) : $${total_price}`
+  total_sp.innerHTML = `${total_product}`
 
 }
-function removeCart(id){
-  card = card.filter((Item)=>
+function removeCart(id) {
+  card = card.filter((Item) =>
     Item.id !== id
   )
-updateCade()
+  updateCade()
 }
-function changenumber(action,id){
+function changenumber(action, id) {
 
-  card = card.map((Item) =>{
+  card = card.map((Item) => {
 
     let numberproduct = Item.numberproduct;
- 
-    if(Item.id === id){
-      
-  if(action === 'minus' && numberproduct >= 1){
-          numberproduct --;
-          console.log(numberproduct)
 
-    }else if(action === "plus"){
-    numberproduct ++;
-    console.log(numberproduct)
+    if (Item.id === id) {
+
+      if (action === 'minus' && numberproduct >= 1) {
+        numberproduct--;
+        console.log(numberproduct)
+
+      } else if (action === "plus") {
+        numberproduct++;
+        console.log(numberproduct)
+      }
     }
-  }
-  
-  return{
-    ...Item,
-numberproduct,
 
-  }
+    return {
+      ...Item,
+      numberproduct,
+
+    }
 
 
-})
-  
+  })
+
   updateCade()
 }
 const nut_hien = document.querySelector('.fa-shopping-cart');
-const cardbuy= document.querySelector('.cardbuy');
-nut_hien.onclick = function(){
+const cardbuy = document.querySelector('.cardbuy');
+nut_hien.onclick = function () {
   cardbuy.classList.toggle('active')
   this.style.back
 
 }
 const imgactive = document.querySelectorAll('.anh');
-for(i = 0; i < imgactive.length; i ++){
-  imgactive[i].onclick = function(){
+for (i = 0; i < imgactive.length; i++) {
+  imgactive[i].onclick = function () {
     var text = this.parentElement.parentElement.querySelector('.text')
     console.log(text)
     console.log(this)
@@ -149,28 +150,28 @@ for(i = 0; i < imgactive.length; i ++){
     text.classList.toggle('active')
   }
 }
-let hearts =  document.querySelectorAll('#icon');
-for( i = 0; i < hearts.length; i++){
-  hearts[i].onclick = function(){
+let hearts = document.querySelectorAll('#icon');
+for (i = 0; i < hearts.length; i++) {
+  hearts[i].onclick = function () {
     this.classList.toggle('icon_change')
-}
+  }
 
 }
-document.querySelector('.fa-search').onclick = () =>{
-    document.querySelector('#form-search').classList.add('active')
+document.querySelector('.fa-search').onclick = () => {
+  document.querySelector('#form-search').classList.add('active')
 
 }
-document.querySelector('#close').onclick = () =>{
-    document.querySelector('#form-search').classList.remove('active')
+document.querySelector('#close').onclick = () => {
+  document.querySelector('#form-search').classList.remove('active')
 
 }
 
 let menu_bar_sanpham = document.querySelector('.fa-bars')
-let nav_sanpham  = document.querySelector('.nav')
-menu_bar_sanpham.onclick= () =>{
-    menu_bar_sanpham.classList.toggle("fa-times")
-    nav_sanpham.classList.toggle('active')
-}  
+let nav_sanpham = document.querySelector('.nav')
+menu_bar_sanpham.onclick = () => {
+  menu_bar_sanpham.classList.toggle("fa-times")
+  nav_sanpham.classList.toggle('active')
+}
 
 
 
--- web ecommerce fontend/js/products.js
+++ web ecommerce fontend/js/products.js
@@ -1,144 +1,144 @@
 const products = [
-   {
-id: 1,
-name:'áo thún tay lỡ united',
-price:500000,
-discription : 'đem đến sự mát mẻ và thoải mái',
-imgSrc :'./images/i1.jpg'
+    {
+        id: 1,
+        name: 'áo thún tay lỡ united',
+        price: 500000,
+        discription: 'đem đến sự mát mẻ và thoải mái',
+        imgSrc: './images/i1.jpg'
     },
     {
         id: 2,
-        name:'áo thun Nike tay lỡ',
-        price:600000,
-        discription : 'phong các trẻ trung nắng động',
-        imgSrc :'./images/i2.jpg'
-            },
-            {
-                id: 3,
-                name:'áo thun Miker xanh lam',
-                price:540000,
-                discription : 'xu hướng trẻ trung và cute',
-                imgSrc :'./images/i3.jpg'
-                    },
-                    {
-                        id: 4,
-                        name:'áo thun  tay lỡ ALLPAY',
-                        price:99000,
-                        discription : 'chất vải mịn hình in sác nét, cute',
-                        imgSrc :'./images/i4.jpg'
-                            },
-                            {
-                                id: 5,
-                                name:'áo polo ngắn tay nữ',
-                                price:495000,
-                                discription : 'Uống trà cải thiện sự tỉnh táo và tư duy',
-                                imgSrc :'./images/i5.jpg'
-                                    },
-                                    {
-                                        id: 6,
-                                        name:'áo polo miker',
-                                        price:750000,
-                                        discription : 'Uống trà cải thiện sự tỉnh táo và tư duy',
-                                        imgSrc :'./images/i6.jpg'
-                                            },
-                                            {
-                                                id: 7,
-                                                name:'áo polo finumk',
-                                                price:564000,
-                                                discription : 'Uống trà cải thiện sự tỉnh táo và tư duy',
-                                                imgSrc :'./images/i7.jpg'
-                                                    },
-                                                    {
-                                                        id: 8,
-                                                        name:'áo polo makernup',
-                                                        price:220000,
-                                                        discription : 'Uống trà cải thiện sự tỉnh táo và tư duy',
-                                                        imgSrc :'./images/i8.jpg'
-                                                            }, {
-                                                                id: 9,
-                                                                name:'áo polo croptop',
-                                                                price:390000,
-                                                                discription : 'Uống trà cải thiện sự tỉnh táo và tư duy',
-                                                                imgSrc :'./images/i9.jpg'
-                                                                    }, 
-                                                                    {
-                                                                        id: 10,
-                                                                        name:'áo polo Suter',
-                                                                        price:910000,
-                                                                        discription : 'Uống trà cải thiện sự tỉnh táo và tư duy',
-                                                                        imgSrc :'./images/i10.jpg'
-                                                                            },
-                                                                            {
-                                                                                id: 11,
-                                                                                name:'áo polo jimek',
-                                                                                price:920000,
-                                                                                discription : 'Uống trà cải thiện sự tỉnh táo và tư duy',
-                                                                                imgSrc :'./images/i11.jpg'
-                                                                                    },
-                                                                                    {
-                                                                                        id: 12,
-                                                                                        name:'áo polo Sukim',
-                                                                                        price:805000,
-                                                                                        discription : 'Uống trà cải thiện sự tỉnh táo và tư duy',
-                                                                                        imgSrc :'./images/i12.jpg'
-                                                                                            },
-                                                                                            {
-                                                                                                id: 8,
-                                                                                                name:'áo thun tay ngắn lenki',
-                                                                                                price:120000,
-                                                                                                discription : 'Uống trà cải thiện sự tỉnh táo và tư duy',
-                                                                                                imgSrc :'./images/i13.jpg'
-                                                                                                    }, {
-                                                                                                        id: 9,
-                                                                                                        name:'áo thun tay ngắn lacker',
-                                                                                                        price:190000,
-                                                                                                        discription : 'Uống trà cải thiện sự tỉnh táo và tư duy',
-                                                                                                        imgSrc :'./images/i15.jpg'
-                                                                                                            }, 
-                                                                                                            {
-                                                                                                                id: 10,
-                                                                                                                name:'áo thun ngắn tay bekuem',
-                                                                                                                price:590000,
-                                                                                                                discription : 'Uống trà cải thiện sự tỉnh táo và tư duy',
-                                                                                                                imgSrc :'./images/i14.jpg'
-                                                                                                                    },
-                                                                                                                    {
-                                                                                                                        id: 11,
-                                                                                                                        name:'áo thun ngắn tay linkpink',
-                                                                                                                        price:499000,
-                                                                                                                        discription : 'Uống trà cải thiện sự tỉnh táo và tư duy',
-                                                                                                                        imgSrc :'./images/i16.jpg'
-                                                                                                                            },
-                                                                                                                            {
-                                                                                                                                id: 12,
-                                                                                                                                name:'áo polo tay lỡ kayzi',
-                                                                                                                                price:695000,
-                                                                                                                                discription : 'Uống trà cải thiện sự tỉnh táo và tư duy',
-                                                                                                                                imgSrc :'./images/i17.jpg'
-                                                                                                                                    },
-                                                                                                                                    {
-                                                                                                                                        id: 10,
-                                                                                                                                        name:'áo polo tay lỡ puka ',
-                                                                                                                                        price:862000,
-                                                                                                                                        discription : 'Uống trà cải thiện sự tỉnh táo và tư duy',
-                                                                                                                                        imgSrc :'./images/i18.jpg'
-                                                                                                                                            },
-                                                                                                                                            {
-                                                                                                                                                id: 11,
-                                                                                                                                                name:'áo polotay lỡ hikic',
-                                                                                                                                                price:974000,
-                                                                                                                                                discription : 'Uống trà cải thiện sự tỉnh táo và tư duy',
-                                                                                                                                                imgSrc :'./images/i19.jpg'
-                                                                                                                                                    },
-                                                                                                                                                    {
-                                                                                                                                                        id: 12,
-                                                                                                                                                        name:'Polo tay lỡ Urooal',
-                                                                                                                                                        price:356000,
-                                                                                                                                                        discription : 'Uống trà cải thiện sự tỉnh táo và tư duy',
-                                                                                                                                                        imgSrc :'./images/i20.jpg'
-                                                                                                                                                            },
-                                                                               
-                                                                       
-                                                  
+        name: 'áo thun Nike tay lỡ',
+        price: 600000,
+        discription: 'phong các trẻ trung nắng động',
+        imgSrc: './images/i2.jpg'
+    },
+    {
+        id: 3,
+        name: 'áo thun Miker xanh lam',
+        price: 540000,
+        discription: 'xu hướng trẻ trung và cute',
+        imgSrc: './images/i3.jpg'
+    },
+    {
+        id: 4,
+        name: 'áo thun  tay lỡ ALLPAY',
+        price: 99000,
+        discription: 'chất vải mịn hình in sác nét, cute',
+        imgSrc: './images/i4.jpg'
+    },
+    {
+        id: 5,
+        name: 'áo polo ngắn tay nữ',
+        price: 495000,
+        discription: 'Uống trà cải thiện sự tỉnh táo và tư duy',
+        imgSrc: './images/i5.jpg'
+    },
+    {
+        id: 6,
+        name: 'áo polo miker',
+        price: 750000,
+        discription: 'Uống trà cải thiện sự tỉnh táo và tư duy',
+        imgSrc: './images/i6.jpg'
+    },
+    {
+        id: 7,
+        name: 'áo polo finumk',
+        price: 564000,
+        discription: 'Uống trà cải thiện sự tỉnh táo và tư duy',
+        imgSrc: './images/i7.jpg'
+    },
+    {
+        id: 8,
+        name: 'áo polo makernup',
+        price: 220000,
+        discription: 'Uống trà cải thiện sự tỉnh táo và tư duy',
+        imgSrc: './images/i8.jpg'
+    }, {
+        id: 9,
+        name: 'áo polo croptop',
+        price: 390000,
+        discription: 'Uống trà cải thiện sự tỉnh táo và tư duy',
+        imgSrc: './images/i9.jpg'
+    },
+    {
+        id: 10,
+        name: 'áo polo Suter',
+        price: 910000,
+        discription: 'Uống trà cải thiện sự tỉnh táo và tư duy',
+        imgSrc: './images/i10.jpg'
+    },
+    {
+        id: 11,
+        name: 'áo polo jimek',
+        price: 920000,
+        discription: 'Uống trà cải thiện sự tỉnh táo và tư duy',
+        imgSrc: './images/i11.jpg'
+    },
+    {
+        id: 12,
+        name: 'áo polo Sukim',
+        price: 805000,
+        discription: 'Uống trà cải thiện sự tỉnh táo và tư duy',
+        imgSrc: './images/i12.jpg'
+    },
+    {
+        id: 8,
+        name: 'áo thun tay ngắn lenki',
+        price: 120000,
+        discription: 'Uống trà cải thiện sự tỉnh táo và tư duy',
+        imgSrc: './images/i13.jpg'
+    }, {
+        id: 9,
+        name: 'áo thun tay ngắn lacker',
+        price: 190000,
+        discription: 'Uống trà cải thiện sự tỉnh táo và tư duy',
+        imgSrc: './images/i15.jpg'
+    },
+    {
+        id: 10,
+        name: 'áo thun ngắn tay bekuem',
+        price: 590000,
+        discription: 'Uống trà cải thiện sự tỉnh táo và tư duy',
+        imgSrc: './images/i14.jpg'
+    },
+    {
+        id: 11,
+        name: 'áo thun ngắn tay linkpink',
+        price: 499000,
+        discription: 'Uống trà cải thiện sự tỉnh táo và tư duy',
+        imgSrc: './images/i16.jpg'
+    },
+    {
+        id: 12,
+        name: 'áo polo tay lỡ kayzi',
+        price: 695000,
+        discription: 'Uống trà cải thiện sự tỉnh táo và tư duy',
+        imgSrc: './images/i17.jpg'
+    },
+    {
+        id: 10,
+        name: 'áo polo tay lỡ puka ',
+        price: 862000,
+        discription: 'Uống trà cải thiện sự tỉnh táo và tư duy',
+        imgSrc: './images/i18.jpg'
+    },
+    {
+        id: 11,
+        name: 'áo polotay lỡ hikic',
+        price: 974000,
+        discription: 'Uống trà cải thiện sự tỉnh táo và tư duy',
+        imgSrc: './images/i19.jpg'
+    },
+    {
+        id: 12,
+        name: 'Polo tay lỡ Urooal',
+        price: 356000,
+        discription: 'Uống trà cải thiện sự tỉnh táo và tư duy',
+        imgSrc: './images/i20.jpg'
+    },
+
+
+
 
 ]
